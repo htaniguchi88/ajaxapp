0ebae8d112e8d126c0f28a94e61464588c3b6994
谷口洋斗 <gucci.taniguchi88@gmail.com>
2019-08-31 00:59:24 +0900

    Fetch data

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("index.js: loaded");
+/*jshint esversion: 6 */
+function fetchUserInfo(userId) {
+	fetch(`https://api.github.com/users/${userId}`)
+		.then(response => {
+				console.log(response.status);
+			if (!response.ok) {
+			} else {
+				response.json().then(userInfo => {
+					console.log(userInfo);
+				});
+			}
+		}).catch(error => {
+				console.error("ネットワークエラー", error);
+		});
+}
